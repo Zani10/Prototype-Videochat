8e0fea9ed457169f29a82bbc6dfde6c2f469dae6
Zani Dobruna <zani.dobruna@student.ehb.be>
2024-12-01 23:53:42 +0100

    Cross-origin error fixed

--- videochat/server/server.js
+++ videochat/server/server.js
@@ -1,19 +1,23 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const cors = require('cors');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: [
-      "http://localhost:5173",
-      process.env.FRONTEND_URL,
-    ],
+    origin: ["https://prototype-videochat-1.onrender.com"],
     methods: ["GET", "POST"],
-    credentials: true
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"]
   }
 });
+
+app.use(cors({
+  origin: "https://prototype-videochat-1.onrender.com",
+  credentials: true
+}));
 
 let waitingUser = null;
 
